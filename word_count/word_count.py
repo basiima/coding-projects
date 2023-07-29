2139e61d8573ace72fb195e1958addbf75daac8a
Julius Basiima <mrbasiima@gmail.com>
2023-07-29 09:51:53 +0000

    count characters in input text/file

--- word_count/word_count.py
+++ word_count/word_count.py
@@ -1,15 +1,23 @@
 """Module to handle command-line arguments and options"""
 import argparse
 
+
 def count_words(text):
     """Function counting words"""
     words = text.split()
     return len(words)
 
+
 def count_lines(text):
     """Function counting lines"""
     lines = text.split("\n")
     return len(lines)
+
+
+def count_characters(text):
+    """Counting characters in input text"""
+    return len(text)
+
 
 def read_text_from_file(file_path):
     """Function reading text from a file"""
@@ -20,10 +28,15 @@ def read_text_from_file(file_path):
         print(f"Error: File '{file_path}' not found.")
         return None
 
-if __name__ == "__main__":  #run the following code only if the code is used as a script
+
+if __name__ == "__main__":  # run the following code only if the code is used as a script
     parser = argparse.ArgumentParser(description="My awesome word count tool")
-    parser.add_argument("input_text", nargs="?", help="The input text for word and line count")
-    parser.add_argument("-l", "--lines", action="store_true", help="Count number of lines")
+    parser.add_argument("input_text", nargs="?",
+                        help="The input text for word and line count")
+    parser.add_argument("-l", "--lines", action="store_true",
+                        help="Count number of lines")
+    parser.add_argument("-c", "--characters",
+                        action="store_true", help="Count characters")
     parser.add_argument("-f", "--file", help="Read input text from file")
     args = parser.parse_args()
 
@@ -41,6 +54,9 @@ if __name__ == "__main__":  #run the following code only if the code is used as 
     if args.lines:
         line_count = count_lines(input_text)
         print("Line count:", line_count)
+    elif args.characters:
+        char_count = count_characters(input_text)
+        print("Characters count:", char_count)
     else:
         word_count = count_words(input_text)
         print("Word count:", word_count)
